b5c191e4bd775d77a7cd011eb3a5d53c62d66db6
邵俊彦 <635178917@qq.com>
2019-11-13 09:48:47 +0800

    fix

--- src/router/asyncRoutes.js
+++ src/router/asyncRoutes.js
@@ -1,25 +1,3 @@
-import Layout from '@/layout'
+// import Layout from '@/layout'
 export const asyncRoutes = [
-  {
-    path: '/form',
-    component: Layout,
-    meta: { title: '表单系统', icon: 'form', roles: ['admin', 'editor'] },
-    redirect: '/form/formindex',
-    children: [{
-      path: 'formindex',
-      name: 'FormIndex',
-      component: () =>
-                import('@/views/form/index'),
-      meta: { title: '表单系统首页', roles: ['admin', 'editor'] }
-    },
-    {
-      path: 'formEdit',
-      name: 'FormEdit',
-      component: () =>
-                import('@/views/form/edit'),
-      meta: { title: '表单系统', roles: ['admin', 'editor'] },
-      hidden: true
-    }
-    ]
-  }
 ]
